1cc709e7f0985e03448b7a759c24f5654cd7cc94
Daniele Piccone <mild.taste@gmail.com>
2015-08-01 09:51:40 +0200

    Adding travis file, fixed tests

--- unit/parsers.unit.rkt
+++ unit/parsers.unit.rkt
@@ -11,7 +11,7 @@
   "Parse query string parameters"
   (define input "GET /hello?foo=bar&baz=foo HTTP/1.1\r\n\r\n\r\n")
   (define parsed-line (parse-request-line input))
-  (check-equal? (hash-ref parsed-line 'verb) "GET")
+  (check-equal? (hash-ref parsed-line 'verb) 'GET)
   (check-equal? (hash-ref parsed-line 'path) "/hello")
   (define parameters (hash-ref parsed-line 'parameters))
   (check-equal? (hash-ref parameters 'foo) "bar")
